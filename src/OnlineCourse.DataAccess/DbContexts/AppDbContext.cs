19238c09a7fccdcd3b855ed1122960a8adf5b1a9
SamandarYigitaliyev <uzgrandmaster@gmail.com>
2023-10-26 13:51:26 +0500

    Created Migration

---
+++ src/OnlineCourse.DataAccess/DbContexts/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineCourse.Domain.Entities.Courses;
+using OnlineCourse.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.DataAccess.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        { }
+        DbSet<UserRole> UserRoles { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Course> Courses { get; set; }
+        DbSet<CourseVideo> CourseVideos { get; set; }
+        DbSet<PurchasedCourse> Purchases { get; set;} 
+    }
+}
---
+++ src/OnlineCourse.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineCourse.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.DataAccess.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        public IQueryable<TEntity> GetAll();
+        public Task<long> CountAsync();
+        public void Add(TEntity entity);
+        public void Update(Guid id, TEntity entity);
+        public void Delete(Guid id);
+        public Task<TEntity?> GetByIdAsync(Guid id);
+    }
+}
