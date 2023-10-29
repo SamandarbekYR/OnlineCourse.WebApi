3d014f37872439bc7bf0495924bc3bbf31cfbda9
SamandarbekYR <SamandarbekYR@gmail.com>
2023-10-30 01:13:25 +0500

    Created Email Controller

---
+++ src/OnlineCourse.Service/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.Service.Dtos.Auth
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Gmail { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ src/OnlineCourse.Service/Services/Auth/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using OnlineCourse.Service.Dtos.Auth;
+using OnlineCourse.Service.Interfaces.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.Service.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        public Task<(bool Result, string Token)> LoginAsync(LoginDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool Result, int CachedMinutes)> RegisterAsync(RegisterDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool Result, string Token)> VerifyRegisterAsync(string phoneNumber, int code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
